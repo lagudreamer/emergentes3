b8ae56a708aef48844d8dd0b681b7ac4077c27bb
lagudreamer <40838434+lagudreamer@users.noreply.github.com>
2018-07-06 12:20:34 +0200

    Update myMongo.js

--- myMongo.js
+++ myMongo.js
@@ -2,35 +2,42 @@ const mng = require('mongoose');
 
 const streamObj = {
     '@context': {
-        name: String,
+        nombre: String,
         creador: String,
-        track: String,
-        uri: String,
-        _dt: String
+        consulta: String,
+        data: String,
+        url: String
+        
     }
 };
 
 const Stream = mng.model('Stream', streamObj);
+
 let promises = mng.connect('mongodb://victor:victor1@ds227481.mlab.com:27481/mydb');
 
-class myMongoDB {
+class myMongo {
 
+    
     constructor(dataDir) {
 
     }
 
+    
     stjson(stream) {
+        
         let str = new Stream(stream);
         str.save();
     }
 
+    
     graphJSLD(callback) {
+        
         Stream.find((err, streams) => {
             if (err) return console.error(err);
             callback(streams);
         })
     }
 
-}//END CLASS
+}
 
-exports.myMongoDB = myMongoDB;
+exports.myMongo = myMongo;
